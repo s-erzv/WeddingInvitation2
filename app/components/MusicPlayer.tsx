6b87fcc0db05e19f329d84c9cb31c905455c26b6
s-erzv <sarahfajriarahmah@gmail.com>
2025-05-12 16:18:41 +0700

    change icon of music player

--- app/components/MusicPlayer.tsx
+++ app/components/MusicPlayer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { FaPlay, FaPause } from 'react-icons/fa';
+import { Volume2, VolumeX } from 'lucide-react';
 
 export default function MusicPlayer() {
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -27,14 +27,12 @@ export default function MusicPlayer() {
     audio.addEventListener('play', handlePlay);
     audio.addEventListener('pause', handlePause);
 
-    // Pause saat pindah tab
     const handleVisibilityChange = () => {
       if (document.hidden) {
         audio.pause();
       }
     };
 
-    // Pause sebelum unload (misalnya tutup tab atau refresh)
     const handleBeforeUnload = () => {
       audio.pause();
     };
@@ -54,7 +52,7 @@ export default function MusicPlayer() {
     <div className="fixed bottom-6 right-6 z-50 bg-white p-3 rounded-full shadow-md">
       <audio ref={audioRef} src="/audio/song.mp3" autoPlay loop />
       <button onClick={togglePlay} className="text-pink-800">
-        {isPlaying ? <FaPause size={20} /> : <FaPlay size={20} />}
+        {isPlaying ? <Volume2 size={20} /> : <VolumeX size={20} />}
       </button>
     </div>
   );
