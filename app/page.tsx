e2e34ee04da2b9c0489e2f7581666953fadefdcc
s-erzv <sarahfajriarahmah@gmail.com>
2025-05-13 10:27:02 +0700

    remove the first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,23 @@
-'use client';
+'use client'
 
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
+import AyatQuran from './components/AyatQuran'
+import CoupleSection from './components/CoupleSection'
+import EventSection from './components/EventSection'
+import Footer from './components/Footer'
+import MusicPlayer from './components/MusicPlayer'
+import Doa from './components/Doa'
+import Hero from './components/Hero'
 
-export default function OpeningPage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/main'); // arahkan ke halaman utama kamu
-  };
-
+export default function MainPage() {
   return (
-    <div className="w-full h-screen bg-[#FBE8E7] flex items-center justify-center px-4">
-      
-      <motion.div
-        initial={{ opacity: 0, scale: 0.95 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-        onClick={handleClick}
-        className="cursor-pointer max-w-3xl w-full"
-      >
-        <Image
-          src="/invitation.svg"
-          alt="Undangan"
-          width={1200}
-          height={800}
-          className="w-full h-auto rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out"
-          priority
-        />
-      </motion.div>
-    </div>
-  );
+    <main>
+      <MusicPlayer />
+      <Hero />
+      <AyatQuran />
+      <CoupleSection />
+      <EventSection />
+      <Doa />
+      <Footer />
+    </main>
+  )
 }
